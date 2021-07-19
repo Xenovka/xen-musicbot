dd799ffd9dfb97965fde57c1ccfa7821363a66a9
Xenovka <wendyanto18@gmail.com>
2021-07-19 11:33:26 +0700

    play command

---
+++ commands/music/play.js
@@ -0,0 +1,38 @@
+const commando = require("discord.js-commando");
+const DisTube = require("distube");
+
+let distube;
+
+module.exports = class Play extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "play",
+      aliases: ["p"],
+      group: "music",
+      memberName: "play",
+      description: "Play a music.",
+      format: "!play [song title] | [youtube url]",
+      argsType: "multiple"
+    });
+
+    distube = new DisTube(client, { leaveOnEmpty: true });
+  }
+
+  async run(message, args) {
+    const { channel, member } = message;
+
+    if (!member.voice.channel) {
+      message.reply(
+        "You must join the voice channel before you can use this command!"
+      );
+      return;
+    }
+
+    if (args.length === 0) {
+      channel.send("Song title or youtube url is required as the arguments!");
+      return;
+    }
+
+    distube.play(message, args.join(" "));
+  }
+};
